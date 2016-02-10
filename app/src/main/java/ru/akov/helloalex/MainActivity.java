2dfc50ce23328f15b05d7f42ba5d26532d929768
Alexander Akovantcev <quidjiboo2015@gmail.com>
2016-02-10 17:23:14 +0300

    добавил много листнеров надо разбираться 0)) минута 6.33 из видео

--- app/src/main/java/ru/akov/helloalex/MainActivity.java
+++ app/src/main/java/ru/akov/helloalex/MainActivity.java
@@ -2,12 +2,44 @@ package ru.akov.helloalex;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.firebase.client.Firebase;
 
 public class MainActivity extends AppCompatActivity {
-
+private static final String FIREBASE_URL = "https://resplendent-inferno-864.firebaseio.com/";
+    private Firebase firebaseRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Firebase.setAndroidContext(this);
+        firebaseRef = new Firebase(FIREBASE_URL);
+
+        EditText inpuText = (EditText) findViewById(R.id.messageText);
+        inpuText.setOnEditorActionListener(new TextView.OnEditorActionListener(){
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_SEND) {
+                    sendMessage();
+                }
+                return true;
+            }
+        });
+        findViewById(R.id.sendButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage();
+            }
+        });
     }
+   public void sendMessage(){
+
+
+    }
+
 }
