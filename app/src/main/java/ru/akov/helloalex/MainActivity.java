2c8163d02a08765f3f7b35117fb7e986f6972109
Alexander Akovantcev <quidjiboo2015@gmail.com>
2016-02-11 11:36:23 +0300

    1)при нажатии на лис он закрывается !  2) назначил метод отправки сообщения через xml вызов onclickmy

--- app/src/main/java/ru/akov/helloalex/MainActivity.java
+++ app/src/main/java/ru/akov/helloalex/MainActivity.java
@@ -1,11 +1,18 @@
 package ru.akov.helloalex;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.firebase.client.Firebase;
@@ -16,6 +23,7 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 private static final String FIREBASE_URL = "https://resplendent-inferno-864.firebaseio.com/";
     private Firebase firebaseRef;
+    private EditText inpuText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +31,8 @@ private static final String FIREBASE_URL = "https://resplendent-inferno-864.fire
         Firebase.setAndroidContext(this);
         firebaseRef = new Firebase(FIREBASE_URL);
 
-        EditText inpuText = (EditText) findViewById(R.id.messageText);
+        inpuText = (EditText) findViewById(R.id.messageText);
+
         inpuText.setOnEditorActionListener(new TextView.OnEditorActionListener(){
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
@@ -33,23 +42,37 @@ private static final String FIREBASE_URL = "https://resplendent-inferno-864.fire
                 return true;
             }
         });
-        findViewById(R.id.sendButton).setOnClickListener(new View.OnClickListener() {
+
+
+        ListView lvMain = (ListView) findViewById(R.id.listViewAkov);
+        lvMain.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View v) {
-                sendMessage();
+            public boolean onTouch(View v, MotionEvent event) {
+                Log.d("dsfsd", "паовпролвап !!!!!!!!!! !!!!!!!!!!!");
+
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(inpuText.getWindowToken(), 0);
+
+                return false;
             }
         });
+
     }
    public void sendMessage(){
+     //  Log.d("dsfsd","паовпролвап !!!!!!!!!! !!!!!!!!!!!");
        EditText Textinput = (EditText) findViewById(R.id.messageText);
-        String message = Textinput.getText().toString();
+        String message = inpuText.getText().toString();
        if(!message.equals("")){
            Random rand = new Random();
            String author = "Tesuser" + rand.nextInt(1000) ;
            ChatmessAlex cMasg = new ChatmessAlex(author,message);
            firebaseRef.push().setValue(cMasg);
-           Textinput.setText("");
+           inpuText.setText("");
        }
+
     }
 
+    public void OnclickMy(View view) {
+        sendMessage();
+    }
 }
